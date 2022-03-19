10d9017d5cc597c36063f565b128f3c356f11d4a
NastmaDzia <100849580+NastmaDzia@users.noreply.github.com>
2022-03-19 16:03:57 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- jurnal4_1302200037/Program.cs
+++ jurnal4_1302200037/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hasil : " + Penjumlahan<int>.JumlahTigaAngka(13, 02, 20));
+
+public class Penjumlahan<T>
+{
+    private T data;
+
+    public Penjumlahan(T data)
+    {
+
+    }
+
+    public static int JumlahTigaAngka(T a1, T a2, T a3)
+    {
+        int A1 = Convert.ToInt32(a1);
+        int A2 = Convert.ToInt32(a2);
+        int A3 = Convert.ToInt32(a3);
+
+        int hasil = A1 + A2 + A3;
+
+        return hasil;
+    }
+}
